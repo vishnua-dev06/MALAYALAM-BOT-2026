2e110f54d703740f2f9098f5d0d55b39e22ae6a0
lolan24 <asianetbot@gmail.com>
2025-03-18 00:08:34 +0530

    Update harifile.py

--- plugins/harifile/harifile.py
+++ plugins/harifile/harifile.py
@@ -2,7 +2,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-@Client.on_message(filters.command("harifile"))
+@Client.on_message(filters.command("link"))
 async def generate_link(client, message):
     command_text = message.text.split(maxsplit=1)
     if len(command_text) < 2:

45489162428f891a5242e1d62b24638f13b0488a
lolan24 <asianetbot@gmail.com>
2025-03-17 22:58:05 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 Updates', url='https://t.me/FILMCORNERALL')
+            InlineKeyboardButton('📢 Updates', url='https://t.me/')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(

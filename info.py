c05098152e81aed78af5a3f8423a4871ba727d10
lolan24 <asianetbot@gmail.com>
2025-10-08 01:58:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,10 @@ AUTO_DEL_TIME = int(300)
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = True 
 PICS = (environ.get('PICS' ,'https://graph.org/file/df17e1fad3d856caf5df8.jpg https://graph.org/file/4db7c2e38f273e3b239dd.jpg https://graph.org/file/e4ead277c0bb7ab445c31.jpg https://graph.org/file/10d81181913c5d4237640.jpg https://graph.org/file/40fef09781fb9ded8cd13.jpg https://graph.org/file/b7b5a32ea575fa7ff50ee.jpg https://graph.org/file/bca1ca2c91828161aa247.jpg https://graph.org/file/bcea1a4ae0176d38a68b7.jpg https://graph.org/file/18a9f945534afa9db5036.jpg https://graph.org/file/89975fb3c157c1ae01e43.jpg https://graph.org/file/c548df4d4a42a8646aa04.jpg')).split()
-
+# Auto Delete Feature
+AUTO_DELETE = True        # enable/disable auto delete
+DELETE_TIME = 300         # time in seconds (300 = 5 minutes)
+DELETE_MESSAGE = True     # send message after deletion
 # Admins, Channels & Users
 OWNER_ID = environ.get('OWNER_ID', '7412083181')
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7412083181').split()]

f650e0f916b9f6d41f4f34363655120a0fbc19fa
lolan24 <asianetbot@gmail.com>
2025-10-09 01:04:22 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -16,6 +16,11 @@ class script(object):
 
 <b>DEVS:</b>
 - <a href=https://github.com/MrMKN>click</a>"""
+
+    CAPTION = """<b><blockquote>{file_caption}
+    
+  ꜰɪʟᴇ ɴᴀᴍᴇ :- {file_name}</b></blockquote>""" 
+    
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
